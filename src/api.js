5c4a5f0556c3aac991e46b025c6203b73aa8615d
Amulya59 <shreeamulya59@gmail.com>
2026-06-24 18:29:13 +0530

    added API endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://paginationbe-10pvhv64j-shreesquad.vercel.app",
 });
 
 export default API;
