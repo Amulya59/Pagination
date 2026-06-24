6bfd2cd6dc3ac9f6f3d3777c8d7a86aad4233e34
Amulya Shankar <104927784+Amulya59@users.noreply.github.com>
2026-06-24 19:32:50 +0530

    Update API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://paginationbe-10pvhv64j-shreesquad.vercel.app",
+  baseURL: "https://paginationbe.vercel.app",
 });
 
 export default API;
